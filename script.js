4e85779b717e2b5a40ed36b3e80c29361a1453d7
Molly McKenzie <mollyamckenzie17@gmail.com>
2025-11-02 13:30:02 -0500

    loader size

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function showSearchStatus(statusText) {
   const loaderDiv = document.createElement('div');
   loaderDiv.className = 'pixel-loader-container';
   loaderDiv.innerHTML = `
-  <svg class="pl pixelated-compass" viewBox="0 0 160 160" width="32" height="32" xmlns="http://www.w3.org/2000/svg" style="image-rendering: pixelated; shape-rendering: crispEdges;">
+  <svg class="pl pixelated-compass" viewBox="0 0 160 160" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
       <defs>
         <linearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
           <stop offset="0%" stop-color="#000"></stop>
